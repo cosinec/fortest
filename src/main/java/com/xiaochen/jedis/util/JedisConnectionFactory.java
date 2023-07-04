d5c06e11644086a31cc571a72130981923a6c681
陈渝璇 <chenyuxuan@eversec.cn>
2023-07-04 14:56:28 +0800

    v1.0

--- src/main/java/com/xiaochen/jedis/util/JedisConnectionFactory.java
+++ src/main/java/com/xiaochen/jedis/util/JedisConnectionFactory.java
@@ -12,7 +12,7 @@ public class JedisConnectionFactory {
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(8);
         poolConfig.setMaxIdle(8);//最大空闲连接
-        poolConfig.setMinIdle(0);//最小空闲连接
+        poolConfig.setMinIdle(0);//最小空闲连接，我再测试
         //创建连接池对象
         jedisPool = new JedisPool(poolConfig,
                 "localhost", 6379, 1000);
